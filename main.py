e546be91c0e763313b5ae2766277bf5f29b8b845
eyal.v <eyal.vosk@sparks-ab.com>
2020-11-16 15:53:16 +0200

    Added partial Gdrive support

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     sel.launch_chrome()
     sel.go_to_decleration_page()
     sel.login(username='', password='')
-    sel.sign_health_decleration()
+    # sel.sign_health_decleration()
     sel.save_screenshot()
     sel.quit()
 
